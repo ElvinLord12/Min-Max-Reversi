e74b6ba1128a5dd0a6d01dc7a7b54077a9ef0b78
Justin Moczynski <jmoczynski@ithaca.edu>
2019-10-23 13:23:01 -0400

    added alpha-beta pruning

--- reversi/reversi_players_jmoczynski.py
+++ reversi/reversi_players_jmoczynski.py
@@ -165,3 +165,46 @@ class MiniMaxPlayerWithPruning:
 
     def get_name(self):
         return str(type(self).__name__)
+
+class MiniMaxPlayerWithABPruning:
+
+    def __init__(self, symbol, alpha, beta):
+        self.symbol = symbol
+        self.alpha = alpha
+        self.beta = beta
+
+    def mini_max_with_pruning(self, board, symbol, depth):
+        moves = board.calc_valid_moves(symbol)
+        if not moves:
+            if board.game_continues():
+                return self.mini_max_with_pruning(board, board.get_opponent_symbol(symbol), depth + 1)
+            else:
+                scores = board.calc_scores()
+                return scores[symbol] - scores[board.get_opponent_symbol(symbol)]
+        else:
+            values = []
+            for move in moves:
+                board_copy = copy.deepcopy(board)
+                board_copy.make_move(symbol, move)
+                values.append(self.mini_max_with_pruning(board_copy, board_copy.get_opponent_symbol(symbol), depth + 1))
+                for value in values:
+                    if value > self.beta:
+                        break
+                    elif value < self.alpha:
+                        break
+                if depth % 2 == 0:
+                    return max(values)
+                else:
+                    return min(values)
+
+    def get_move(self, board):
+        moves = board.calc_valid_moves(self.symbol)
+        values = []
+        for move in moves:
+            board_copy = copy.deepcopy(board)
+            board_copy.make_move(self.symbol, move)
+            values.append(self.mini_max_with_pruning(board_copy, self.symbol, 1))
+        return moves[values.index(max(values))]
+
+    def get_name(self):
+        return str(type(self).__name__)
